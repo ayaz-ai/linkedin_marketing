1ecd8fe6fc1f77ea8033c141d1ed20896e2a22c1
Ayaz <ayazreshamwala@outlook.com>
2023-08-02 22:59:57 +0530

    Name change and keys removed

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ from batch_processor import loop_engine
 if __name__ == "__main__":
     # Provide Input File Path
     url_list = csv_to_list(file_path='input.csv')
-    data_json = loop_engine(url_list, api_key='_ToPH59TfbXbvu2bEgWYJw')
+    data_json = loop_engine(url_list, api_key='')
     print(json.dumps(data_json, indent=4))
