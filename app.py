a41b559f7bb8253eeb9b40d671c2a8649f8008f2
Ayaz <ayazreshamwala@outlook.com>
2023-08-16 12:35:53 +0530

    output log added

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from batch_processor import loop_engine
 
 if __name__ == "__main__":
     # Provide Input File Path
+    output_file = "output_log.json"
     url_list = csv_to_list(file_path='input.csv')
-    data_json = loop_engine(url_list, api_key='')
+    data_json = loop_engine(url_list, output_file, api_key='')
     print(json.dumps(data_json, indent=4))
     json_to_csv(data_json)
--- batch_processor.py
+++ batch_processor.py
@@ -1,24 +1,29 @@
+import json
 from contact_number_api import contact_phone
 from selenium_driver import email_fetcher
 
-data_dictionary = {}
+def loop_engine(url_list, output_file, api_key):
+    data_dictionary = {}
 
-def loop_engine(url_list, api_key):
-    
     for url in url_list:
         if url not in data_dictionary:
             data_dictionary[url] = {}
-        # contact_api_hit(url, api_key)
-        web_selenium(url)
-
+        
+        # data_dictionary = contact_api_hit(data_dictionary, url, api_key)
+        data_dictionary = web_selenium(data_dictionary, url)
+        
+        with open(output_file, 'a') as json_file:
+            json.dump({url: data_dictionary[url]}, json_file)
+            json_file.write('\n')
+    
     return data_dictionary
 
-def contact_api_hit(url, key):
+def contact_api_hit(data_dictionary, url, key):
     contact = contact_phone(url, key)
     data_dictionary[url]['number'] = contact['numbers']
     return data_dictionary
 
-def web_selenium(url):
+def web_selenium(data_dictionary, url):
     name, email, title, company_name, website, hq_phone, industry, company_size, headquaters = email_fetcher(url)
     data_dictionary[url]['email'] = email
     data_dictionary[url]['name'] = name
